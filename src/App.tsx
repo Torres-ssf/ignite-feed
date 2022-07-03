e962cbc2d52a0b0598145e77d2aed6d53eba0196
Torres-ssf <sergio.uft@gmail.com>
2022-07-03 16:34:02 -0300

    add sidebar component to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import { Header } from './components/Header'
-
 import './global.css'
+
+import styles from './App.module.css'
+import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
 
 export function App () {
   return (
+    <>
     <Header />
+      <div className={styles.wrapper}>
+        <Sidebar/>
+      </div>
+    </>
+
   )
 }
