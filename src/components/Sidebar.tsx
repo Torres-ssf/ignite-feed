fe2de3f35f49fdc967cc26f369565126d1fa7c25
Torres-ssf <sergio.uft@gmail.com>
2022-07-04 18:32:00 -0300

    use avatar component on sidebar component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,6 @@
 import { PencilLine } from 'phosphor-react'
 
+import { Avatar } from './Avatar'
 import styles from './Sidebar.module.css'
 
 export function Sidebar () {
@@ -10,14 +11,13 @@ export function Sidebar () {
         src="https://images.unsplash.com/photo-1526040652367-ac003a0475fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
 
-      <span className={styles.profile}>
-        <img
-          className={styles.avatar}
+      <div className={styles.profile}>
+        <Avatar
           src="https://avatars.githubusercontent.com/Torres-ssf"
         />
         <strong>Sergio Torres</strong>
-        <p>Software Engineer</p>
-      </span>
+        <span>Software Engineer</span>
+      </div>
 
       <footer>
         <a href="#">
