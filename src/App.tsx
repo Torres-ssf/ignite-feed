fca5e517fbe8954026f4f74933d5ccddb08e24e8
Torres-ssf <sergio.uft@gmail.com>
2022-07-04 18:34:40 -0300

    set up post component on app component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './global.css'
 
 import styles from './App.module.css'
 import { Header } from './components/Header'
+import { Post } from './components/Post'
 import { Sidebar } from './components/Sidebar'
 
 export function App () {
@@ -10,6 +11,9 @@ export function App () {
     <Header />
       <div className={styles.wrapper}>
         <Sidebar/>
+        <main>
+          <Post />
+        </main>
       </div>
     </>
 
