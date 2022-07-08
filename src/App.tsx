aa21804b72d3bfc5fac8986553b19008644bd605
Torres-ssf <sergio.uft@gmail.com>
2022-07-08 14:54:11 -0300

    refact static data to conform with changes on post component

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,72 @@ import './global.css'
 
 import styles from './App.module.css'
 import { Header } from './components/Header'
-import { Post } from './components/Post'
+import { IPostProps, Post } from './components/Post'
 import { Sidebar } from './components/Sidebar'
 
 export function App () {
+  const posts: IPostProps[] = [
+    {
+      id: '0',
+      author: {
+        avatar: 'https://avatars.githubusercontent.com/Torres-ssf',
+        name: 'Torres-ssf',
+        role: 'Software Engineer',
+      },
+      publishedAt: new Date(),
+      content: {
+        text: `Fala galeraa 👋
+        
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+          
+          👉 {{link-0}}
+
+          {{hashtag-0}} {{hashtag-1}} {{hashtag-2}}
+        `,
+        links: ['jane.design/doctorcare'],
+        hashtags: ['newproject', 'nlw', 'rocketseat'],
+      },
+      comments: [
+        {
+          id: '0',
+          author: {
+            avatar: 'https://avatars.githubusercontent.com/Torres-ssf',
+            name: 'Torres-ssf',
+            role: 'Software Engineer',
+          },
+          likes: 10,
+          publishedAt: new Date(),
+          content: {
+            text: `Fala galeraa 👋
+            
+              Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+              
+              👉 {{link-0}}
+    
+              {{hashtag-0}} {{hashtag-1}} {{hashtag-2}}
+            `,
+            links: ['jane.design/doctorcare'],
+            hashtags: ['newproject', 'nlw', 'rocketseat'],
+          },
+        },
+      ],
+    },
+  ]
+
   return (
     <>
-    <Header />
+      <Header />
       <div className={styles.wrapper}>
         <Sidebar/>
         <main>
-          <Post />
+          {posts.map(post => {
+            return <Post
+              key={post.id}
+              {...post}
+            />
+          })}
         </main>
       </div>
     </>
-
   )
 }
