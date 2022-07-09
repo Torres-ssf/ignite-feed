4c3ca3ded134f81ce1afb414e0a5718fbc8d7d9a
Torres-ssf <sergio.uft@gmail.com>
2022-07-08 21:14:01 -0300

    define more regexs

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -12,3 +12,28 @@ export const splitLinkOrHashtagRegex = /\{\{|-|\}\}/
  * Should test if url starts with http:// or https://
  */
 export const testLinkProtocolRegex = /^(https?:\/\/)/
+
+/**
+ * Should match any HTML tag insige the text
+ */
+export const filterHtmlRegex = /(<([^>]+)>)/g
+
+/**
+ * Should match any link inside the text
+ */
+export const filterLinksRegex = /([^\S]|^)(((https?:\/\/)|(www\.)|([a-z0-9-]+\.))(\S+))/gi
+
+/**
+ * Should match any hashtag inside the text
+ */
+export const filterHashtagsRegex = /#\w+/gi
+
+/**
+ * Should match empty spaces before and after the text
+ */
+export const trimRegex = /^\s+|\s+$/g
+
+/**
+ * Should match string junks with more than 2 \n
+ */
+export const filterMultipleEmptyLines = /\n{3,}/g
